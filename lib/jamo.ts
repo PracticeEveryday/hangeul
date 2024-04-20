baba0f56123caa51818efa296fbd726f6b6a07b6
PracticeEveryday <developerforus@gmail.com>
2024-04-20 17:17:21 +0900

    feat: 한글의 복합 자음 모음도 분리하는 함수 추가

--- lib/jamo.ts
+++ lib/jamo.ts
@@ -1,19 +1,30 @@
-import { choseong, jungseong, jongseong, HANGEUL_UNICODE_INDEX } from "./const/jamo.const";
-import {isHangeul} from "./validate";
+import {
+    choseong,
+    jungseong,
+    jongseong,
+    HANGEUL_UNICODE_INDEX,
+    gyeopbatchimObj,
+    bokhapJaeumObj,
+    bokhapMoeumObj
+} from "./const/jamo.const";
+import {isHangeul, isJaeumMoeum} from "./validate";
 
 export function convertKrToJamoArr(str: string) {
-    if(!isHangeul(str)) throw new Error("this is not hangeul");
+    if(!isHangeul(str)) throw new Error("한글이 아닙니다.");
+    if(isJaeumMoeum(str)) return [str];
 
     let jamoArray = <string[]>[];
 
     for (let char of str) {
         let unicode = char.charCodeAt(0) - HANGEUL_UNICODE_INDEX;
-        const choIdx = Math.floor(unicode / 588);
-        const jungIdx = Math.floor((unicode % 588) / 28);
-        const jongIdx = unicode % 28;
 
+        const choIdx = Math.floor(unicode / 588);
         jamoArray.push(choseong[choIdx]);
+
+        const jungIdx = Math.floor((unicode % 588) / 28);
         jamoArray.push(jungseong[jungIdx]);
+
+        const jongIdx = unicode % 28;
         jongIdx !== 0 && jamoArray.push(jongseong[jongIdx]);
     }
 
@@ -21,8 +32,21 @@ export function convertKrToJamoArr(str: string) {
 }
 
 export function convertKrToFullJamoArr(str: string) {
-    if(!isHangeul(str)) throw new Error("this is not hangeul");
-    const jamoArray = convertKrToJamoArr(str);
+    if(!isHangeul(str)) throw new Error("한글이 아닙니다.");
+    const jamoArr = convertKrToJamoArr(str);
 
+    const result = <string[]>[]
+    for(let i = 0; i < jamoArr.length; i++) {
+        if(Object.prototype.hasOwnProperty.call(bokhapJaeumObj, jamoArr[i])) {
+            result.push(...bokhapJaeumObj[jamoArr[i]]);
+        } else if(Object.prototype.hasOwnProperty.call(bokhapMoeumObj, jamoArr[i])) {
+            result.push(...bokhapMoeumObj[jamoArr[i]]);
+        } else if(Object.prototype.hasOwnProperty.call(gyeopbatchimObj, jamoArr[i])) {
+            result.push(...gyeopbatchimObj[jamoArr[i]]);
+        } else {
+            result.push(jamoArr[i]);
+        }
+    }
 
+    return result;
 }
--- lib/validate.ts
+++ lib/validate.ts
@@ -1,5 +1,11 @@
+import {choseong, jongseong, jungseong} from "./const/jamo.const";
+
 export const isHangeul = (str: string) =>{
     let checkKor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
 
     return checkKor.test(str);
 };
+
+export const isJaeumMoeum  = (str: string) => {
+    return (choseong.includes(str) || jongseong.includes(str) || jungseong.includes(str));
+}
